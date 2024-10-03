39f8c2803457c062d89b164ab5b7841fe86e50f4
rajendrachy <chyrajendra32@gmail.com>
2024-10-04 00:47:43 +0530

    added

---
+++ FEE LAB/IMAGE SLIDER/script.js
@@ -0,0 +1,10 @@
+// Select all image elements
+let images = document.querySelectorAll('#open');
+
+// Add click event listener to each image
+images.forEach(image => {
+    image.addEventListener('click', function(open) {
+
+        
+    });
+});
---
+++ FEE LAB/POP UP/script.js
@@ -0,0 +1,13 @@
+let popup = document.querySelector(".popup");
+
+
+
+function openPopup () {
+    popup.classList.add("open-popup");
+}
+
+
+
+function closePopup () {
+    popup.classList.remove("open-popup");
+}
